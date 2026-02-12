94b6927259632555f0e72a1aabae04187e77510a
jhonrneilebuan <160509990+jhonrneilebuan@users.noreply.github.com>
2026-02-12 10:22:52 +0800

    update the vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/jhonrneil_resume/" : "/",
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
+export default defineConfig(({ mode }) => {
+  const isVercel = !!process.env.VERCEL;
+  const isProd = mode === "production";
+
+  return {
+    base: isProd ? (isVercel ? "/" : "/jhonrneil_resume/") : "/",
+    plugins: [react()],
+    server: {
+      host: "::",
+      port: 8080,
+      hmr: { overlay: false },
     },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-}));
+  };
+});
